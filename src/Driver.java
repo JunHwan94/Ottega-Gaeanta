2fe1ab46c821b7e3876cc3551d80f0562dcb0c3d
ubuntu <vkffkehlss@gmail.com>
2021-08-30 18:50:36 -0700

    add files again
    Former-commit-id: 2ea49d2b9f6ea81107a05690e783648158613908

--- src/Driver.java
+++ src/Driver.java
@@ -7,8 +7,9 @@ public class Driver {
 		int exitCode = -1;
 		ProgramDriver pgd = new ProgramDriver();
 		try {
-			pgd.addClass("wordcount",Wordcount.class,"");
-			pgd.addClass("kmeans",Kmeans.class,"");
+			pgd.addClass("wordcount", Wordcount.class,"");
+			pgd.addClass("kmeans", Kmeans.class,"");
+			pgd.addClass("wordcount1char", Wordcount1char.class, "");
       			pgd.driver(args);
 			exitCode = 0;
 		}
