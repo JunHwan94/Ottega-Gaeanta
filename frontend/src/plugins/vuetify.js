72d75da7c2dc41d1c08a883ae492744ae81b8730
jongs3030 <jongs3030@naver.com>
2021-09-11 10:20:49 -0700

    Add: set frontend vue vuetify initial develop environment
    Former-commit-id: 39f4ce54e519cbbbd07bf51427796591aef27e0a

---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+// 아래꺼 can't find modules 경고 떠서 일단 주석처리했습니다.
+// import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+});
