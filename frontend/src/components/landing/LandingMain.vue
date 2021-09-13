71c031901c53adbab9e9ee2570d2efc24eb0cf23
이재영 <dldustn14@gmail.com>
2021-09-13 23:00:51 +0900

    test: 렌딩페이지 slider 기능 테스트
    Former-commit-id: e726278fbd185fac1d55f080761921b7b6c6f9f7

--- frontend/src/components/landing/LandingMain.vue
+++ frontend/src/components/landing/LandingMain.vue
@@ -30,17 +30,19 @@
   width: 90%;
   height: 93vh;
 }
-.text {
-  font-weight: bold;
-  color: white;
-  width: 100%;
-}
 .main-title {
   left: 150px;
   top: 100px;
   font-size: 4rem;
   position: absolute;
   text-align: initial;
+  animation: fadeIn 1.5s ease-in-out;
+}
+.text {
+  font-weight: bold;
+  color: white;
+  width: 100%;
+  animation: fadeIn 2.0s ease-in-out;
 }
 .main-text {
   left: 150px;
@@ -48,5 +50,15 @@
   font-size: 2rem;
   position: absolute;
   text-align: initial;
+}
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: translateY(-50px);
+  }
+  to {
+    opacity: 3;
+    transform: none;
+  }
 }
 </style>
