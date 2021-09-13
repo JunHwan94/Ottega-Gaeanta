e553d01ec4659abe18bebefeaf38a39ea6441878
이재영 <giver12@naver.com>
2021-09-13 11:13:00 +0900

    refactor: vuex 모듈화 / state, actions, mutations, getters
    Former-commit-id: fe35b6c56e78f64f4be0388103e49848b2424dee

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import state from './modules/state'
+import * as actions from './modules/actions'
+import * as mutations from './modules/mutations'
+import * as getters from './modules/getters'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  namespaced: true,
+  state,
+  mutations,
+  getters,
+  actions,
+  modules: {}
 })
---
+++ frontend/src/store/modules/state.js
@@ -0,0 +1,6 @@
+const test = '텟트 입니다.'
+export default function () {
+    return {
+        test,
+    }
+}
