3be16024f7b16439946111011a24908224f761a6
이재영 <dldustn14@gmail.com>
2021-09-13 15:26:27 +0900

    add: navbar.vue 구성 및 추가
    Former-commit-id: d0f426de0ce2071444e9f26a451002c388f04210

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="left">
+        <h2>Ottega-Gaeanta</h2>
+      </div>
+      <div class="right" justify-end>
+        <v-menu v-for="([text, rounded], index) in btns" :key="text" :rounded="rounded" offset-y>
+          <template v-slot:activator="{ attrs, on }">
+            <v-btn :color="colors[index]" class="white--text" v-bind="attrs" v-on="on">
+              메뉴
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item v-for="item in items" :key="item" link>
+              <v-list-item-title v-text="item"></v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </div>
+    <br>
+    <v-divider></v-divider>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+    data: () => ({
+      btns: [
+        ['Large', 'lg'],
+      ],
+      colors: ['teal darken-1'],
+      items: [...Array(4)].map((_, i) => `Item ${i}`),
+    }),
+  }
+</script>
+
+<style scoped>
+.header {
+  padding: 20px;
+  padding-left: 3%;
+  padding-right: 3%;
+  width: 100%;
+}
+.left {
+  width: 50%;
+  float: left;
+}
+.right {
+  width: 50%;
+  float: right;
+  text-align: right;
+}
+</style>
