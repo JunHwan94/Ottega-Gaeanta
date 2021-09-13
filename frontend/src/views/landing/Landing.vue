47bab34d98270be8bea708918b50a8d4b9bce885
jongs3030 <jongs3030@naver.com>
2021-09-13 13:32:54 +0900

    Add: directory structure and router setting done
    Former-commit-id: 0c19377bb9bcd3623b030dababfc9934bce589b5

---
+++ frontend/src/views/landing/Landing.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    랜딩페이지 구현
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/search/SearchItems.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    검색 키워드 모음 (카테고리, 디테일)
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
