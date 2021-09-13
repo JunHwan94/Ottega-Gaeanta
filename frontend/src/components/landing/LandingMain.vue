e08a33234ad9ca2be9beaac9ee2275c2bde4a795
이재영 <giver12@naver.com>
2021-09-13 19:58:20 +0900

    test: 렌딩 페이지 swiper lib 테스트
    Former-commit-id: b17188adefa138ab33b3e47811b3809f65d0801a

---
+++ frontend/src/components/landing/LandingMain.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main">
+    <img class="main-image" :src="mainImageUrl"/>
+    <div class="main-title text">
+      어? 너<br>
+      옷태가 괜찮다?
+    </div>
+    <div class="main-text text">
+      패션 피플이 선택한 AI 의류 추천 서비스<br>
+      120만 건이 의류 조합을 한 번에!
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LandingMain',
+    data: () => ({
+      mainImageUrl: 'https://www.10wallpaper.com/wallpaper/1366x768/1611/Girl_in_white_dress-Celebrity_Poster_Wallpapers_1366x768.jpg',
+    }),
+  }
+</script>
+
+<style scoped>
+.main {
+  text-align: center;
+  position: relative;
+}
+.main-image {
+  width: 90%;
+  height: 93vh;
+}
+.text {
+  font-weight: bold;
+  color: white;
+  width: 100%;
+}
+.main-title {
+  left: 150px;
+  top: 100px;
+  font-size: 4rem;
+  position: absolute;
+  text-align: initial;
+}
+.main-text {
+  left: 150px;
+  top: 600px;
+  font-size: 2rem;
+  position: absolute;
+  text-align: initial;
+}
+</style>
---
+++ frontend/src/components/landing/LandingSlider.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper> -->
+  <div>dsa</div>
+</template>
+
+<script>
+  // import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+  // import 'swiper/css/swiper.css'
+  // import "swiper/swiper-bundle.css"
+  export default {
+    name: 'LandingSlider',
+    components: {
+    },
+    data: () => ({
+    }),
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
