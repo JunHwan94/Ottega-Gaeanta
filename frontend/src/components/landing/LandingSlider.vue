d234c5117635b11e43c4a5030839c8eee5ecdb89
이재영 <giver12@naver.com>
2021-09-14 14:14:13 +0900

    test: 렌딩 페이지 레이아웃 구조 작성
    Former-commit-id: 191c2bf819eedbbaad551b05525418e371c20427

--- frontend/src/components/landing/LandingSlider.vue
+++ frontend/src/components/landing/LandingSlider.vue
@@ -2,7 +2,7 @@
   <div class="slider">
     <div>
       <v-container class="lighten-5">
-        <v-row class="mb-12" no-gutters>
+        <v-row class="mb-12 menu" no-gutters>
           <v-col lg="3">
             스마트 검색
           </v-col>
@@ -17,17 +17,84 @@
           </v-col>
         </v-row>
       </v-container>
-      <v-container class="lighten-5">
+      <v-container class="lighten-5 menu-container">
+        <!-- 똑똑하게 검색하기 소개 -->
         <v-row class="mb-6" justify="center" no-gutters>
           <v-col lg="4">
-            <img :src="image" />
+            <img :src="image[0]" />
           </v-col>
+          <v-col lg="1"></v-col>
+          <v-col class="menu-detail" lg="4">
+            <div class="menu-detail-title">똑똑하게 검색하기</div>
+            <div class="menu-detail-content">
+              오늘 입고나갈 옷👚은 정하셨나요?<br>
+              다른 사람들은 어떻게 옷을 입는지 구경하고<br>
+              나만의 스타일을 만들어가세요.
+            </div>
+            <div class="menu-btn">
+              스타일 똑똑하게 검색하세요!
+            </div>
+          </v-col>
+        </v-row>
+
+        <!-- 빅데이터로 정확하게 소개 -->
+        <v-row class="mb-6" justify="center" no-gutters>
           <v-col lg="4">
-            안녕하세요
+            <img :src="image[1]" />
+          </v-col>
+          <v-col lg="1"></v-col>
+          <v-col class="menu-detail" lg="4">
+            <div class="menu-detail-title">빅데이터로 정확하게!</div>
+            <div class="menu-detail-content">
+              120만건이 패션 스타일을 분석하여<br>
+              어울리는 스타일과 색상을 알려드려요!
+            </div>
+            <div class="menu-btn">
+              나만의 코디를 추천 받고싶다면?
+            </div>
+          </v-col>
+        </v-row>
+
+        <!-- 퍼스널 컬러 기능 소개 -->
+        <v-row class="mb-6" justify="center" no-gutters>
+          <v-col lg="4">
+            <img :src="image[2]" />
+          </v-col>
+          <v-col lg="1">
+          </v-col>
+          <v-col class="menu-detail" lg="4">
+            <div class="menu-detail-title">내 피부에 날개를 달아줘</div>
+            <div class="menu-detail-content">
+              나이 피부톤을 정확하게 알고 자신만의<br>
+              개성있는 스타일을 만들어가세요!
+            </div>
+            <div class="menu-btn">
+              나이 퍼스널 컬러 알아보기
+            </div>
+          </v-col>
+        </v-row>
+
+        <!-- 서비스 시작하기 -->
+        <v-row class="mb-6" justify="center" no-gutters>
+          <v-col lg="4">
+            <img :src="image[3]" />
+          </v-col>
+          <v-col lg="1">
+          </v-col>
+          <v-col class="menu-detail" lg="4">
+            <div class="menu-detail-title">지금 바로 둘러보기</div>
+            <div class="menu-detail-content">
+              지금 바로 옷태가 괜찮네 서비스에<br>
+              접속하고 다양한 기능을 경험하세요.
+            </div>
+            <div class="menu-btn">
+              서비스 시작하기
+            </div>
           </v-col>
         </v-row>
       </v-container>
-      <div class="progressContainer">
+      <div class="progress-container">
+        <div class="progress" @click="nextPage"/>
         <div class="progress" @click="nextPage"/>
         <div class="progress" @click="nextPage"/>
         <div class="progress" @click="nextPage"/>
@@ -42,7 +109,18 @@
     name: 'LandingSlider',
     components: {},
     data: () => ({
-      image: 'https://img1.daumcdn.net/thumb/R1280x0.fpng/?fname=http://t1.daumcdn.net/brunch/service/user/6x6e/image/mG_H5qGH5RElp6kLQGHG0sheyG8.png',
+      image: [
+        'https://img1.daumcdn.net/thumb/R1280x0.fpng/?fname=http://t1.daumcdn.net/brunch/service/user/6x6e/image/mG_H5qGH5RElp6kLQGHG0sheyG8.png',
+        'https://static.coupangcdn.com/image/vendor_inventory/5fdd/3635f2d30ae5299adad49ac0d61cc2cf47a02d5c1f70d3e37db5f9ed1d54.jpg',
+        'https://lh3.googleusercontent.com/proxy/LLxe4nMUgm83UjeZrDOMpsdVNPCA-X6sjcX113hVN5tgrymZ8rfOcpYtU17Dm9sX-tZk_8mi4SUIcCZWTNjfxwGqVxe-Wwnb9TE8PjeaGGJzJNLzoublBTb4Hr5fNYvwJgNeRBtagdBpMugfXpwTa466Jj6pqI_mjvDcv2b23WiWkvrMH64',
+        'https://post-phinf.pstatic.net/MjAyMTA0MDZfMjIw/MDAxNjE3Njk0NTYzMDIy.vh_iFhI2K6VWGCSX0ysqFr23aPnYqPTZkzpbUav7xRkg.AWMAI1T96OnWsGWPDDwHrkNCAgOlBho_S9cqzhArFg8g.JPEG/%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8_%EB%8F%84%EC%8B%9D%ED%99%94.jpg?type=w1200'
+      ],
+      btnColor: [
+        '#BAD7DF',
+        '#FFE2E2',
+        '#99DDCC',
+        '#FFE2E2',
+      ],
       page: 1,
     }),
     methods: {
@@ -67,14 +145,17 @@
     font-weight: normal;
     font-style: normal;
 }
-div {
-  font-family: 'HSYuji-Regular';
-  color: hotpink;
+@font-face {
+    font-family: 'Cafe24Ssurround';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/Cafe24Ssurround.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
 }
 img {
   width:25vw;
+  height: 250px;
 }
-.progressContainer {
+.progress-container {
   text-align: center;
 }
 .progress {
@@ -83,10 +164,46 @@ img {
   height: 10px;
   border-radius: 10px;
   background: rgb(194, 191, 191);
-  margin: 10px 10px;
+  margin: 5px 5px;
   cursor: pointer;
 }
 .progress:hover {
   box-shadow: 0 0 5px #FF0000;
+}
+.menu {
+  font-family: 'HSYuji-Regular';
+  font-size: 1.5rem;
+  color: hotpink;
+}
+.menu-detail {
+  font-family: Cafe24Ssurround;
+  text-align: left;
+}
+.menu-detail-title {
+  font-size: 2rem;
+}
+.menu-detail-content {
+  margin-top: 1.5rem;
+  font-size: 1.2rem;
+}
+.menu-btn {
+  text-align: center;
+  margin-top: 3rem;
+  background-color: #FFE2E2;
+  border-radius: 20px;
+  padding: 10px;
+}
+.menu-container {
+  animation: fadeIn 1.0s ease-in-out;
+}
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 3;
+    transform: none;
+  }
 }
 </style>
