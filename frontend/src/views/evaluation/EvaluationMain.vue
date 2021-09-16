f23a4eae52cfe24f6a0feab63e5b85346f383cf9
이재영 <dldustn14@gmail.com>
2021-09-16 17:08:51 +0900

    test: 의상 평가 메뉴 레이아웃 구성  - 실시간 의상 측정  - 사진업로드  - 색상 조합 메뉴 구성
    Former-commit-id: a6701b6936ebc83f5e8a5ce913992d96513c0a5b

--- frontend/src/views/evaluation/EvaluationMain.vue
+++ frontend/src/views/evaluation/EvaluationMain.vue
@@ -6,30 +6,51 @@
       </v-row>
     </v-container>
     <div class="card">
-      <!-- <v-for key="" -->
       <v-row class="menu-container" justify="center">
-        <div class="menu">
-          사진 촬용
-        </div>
-      </v-row><br>
-      <v-row class="menu-container" justify="center">
-        <div class="menu">
-          이미지 업로드
-        </div>
-      </v-row><br>
-      <v-row class="menu-container" justify="center">
-        <div class="menu">
-          색 조합표
-        </div>
+        <v-col lg=4>
+          <img class="left-image" style="width:400px; height:400px; visibility: hidden;" />
+        </v-col>
+        <v-col lg=4>
+          <v-row class="menu-container" justify="center" @click="changePage(0)">
+            <div class="menu">
+              실시간 측정 방식
+              <v-icon large color="white darken-5">mdi-selection</v-icon>
+            </div>
+          </v-row><br>
+          <v-row class="menu-container" justify="center">
+            <div class="menu">
+              사진 업로드 방식
+              <v-icon large color="white darken-5"> mdi-folder-star</v-icon>
+            </div>
+          </v-row><br>
+          <v-row class="menu-container" justify="center">
+            <div class="menu">
+              색 조합표 보기
+              <v-icon large color="white darken-5"> mdi-arrow-up-bold-box-outline</v-icon>
+            </div>
+          </v-row>
+        </v-col>
+        <v-col lg=4>
+          <img class="right-image" style="width:400px; height:400px; visibility: hidden;" />
+        </v-col>
       </v-row>
-      <!-- 이미지로 버튼 눌림 효과 -->
-      <!-- <v-row justify="center" no-gutters>
-        <div class="photo">
-          <img src="https://s-media-cache-ak0.pinimg.com/736x/06/d1/5f/06d15fc8a69e2ab67143e5357a4184d7.jpg"
-            style="width: 100px; height: 100px;" />
-          <div class="glow-wrap">
-            <i class="glow"></i>
-          </div>
+      <!-- <img class="main-image" :src="mainImageUrl" style="width:100px; height:100px;"/>
+      <v-row class="menu-container" justify="center">
+        <div class="menu">
+          실시간 측정 방식
+          <v-icon large color="white darken-5">mdi-selection</v-icon>
+        </div>
+      </v-row><br>
+      <v-row class="menu-container" justify="center">
+        <div class="menu">
+          사진 업로드 방식
+          <v-icon large color="white darken-5"> mdi-folder-star</v-icon>
+        </div>
+      </v-row><br>
+      <v-row class="menu-container" justify="center">
+        <div class="menu">
+          색 조합표 보기
+          <v-icon large color="white darken-5"> mdi-arrow-up-bold-box-outline</v-icon>
         </div>
       </v-row> -->
     </div>
@@ -39,128 +60,97 @@
 <script>
   export default {
     data: () => ({
-      menus: [
-        { name: '사진 촬영'},
-        { name: '이미지 업로드'}, 
-        { name: '색 조합표'},
-      ]
+      pages: ['evaluationFilm'],
+      mainImageUrl: '',
     }),
     mounted() {
-      const menu = document.querySelector('.menu')
-      // const card = document.querySelector('.card')
-      menu.onmouseover = function() {
-        // alert('test')
-        // card.style.backgroundImage = url('https://webisfree.com/static/uploads/2019/0153_images886.jpg')
+      const menu = document.querySelectorAll('.menu')
+      const leftImage = document.querySelector('.left-image')
+      const rightImage = document.querySelector('.right-image')
+      menu[0].addEventListener('mouseover', function () {
+        imageClear()
+        leftImage.src =
+          'https://post-phinf.pstatic.net/MjAyMTA0MDZfMjIw/MDAxNjE3Njk0NTYzMDIy.vh_iFhI2K6VWGCSX0ysqFr23aPnYqPTZkzpbUav7xRkg.AWMAI1T96OnWsGWPDDwHrkNCAgOlBho_S9cqzhArFg8g.JPEG/%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8_%EB%8F%84%EC%8B%9D%ED%99%94.jpg?type=w1200'
+        leftImage.style.visibility = 'visible'
+      })
+      menu[1].addEventListener('mouseover', function () {
+        imageClear()
+        rightImage.src =
+          'https://post-phinf.pstatic.net/MjAyMTA0MDZfMjIw/MDAxNjE3Njk0NTYzMDIy.vh_iFhI2K6VWGCSX0ysqFr23aPnYqPTZkzpbUav7xRkg.AWMAI1T96OnWsGWPDDwHrkNCAgOlBho_S9cqzhArFg8g.JPEG/%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8_%EB%8F%84%EC%8B%9D%ED%99%94.jpg?type=w1200'
+        rightImage.style.visibility = 'visible'
+      })
+      menu[2].addEventListener('mouseover', function () {
+        imageClear()
+        rightImage.src =
+          'https://post-phinf.pstatic.net/MjAyMTA0MDZfMjIw/MDAxNjE3Njk0NTYzMDIy.vh_iFhI2K6VWGCSX0ysqFr23aPnYqPTZkzpbUav7xRkg.AWMAI1T96OnWsGWPDDwHrkNCAgOlBho_S9cqzhArFg8g.JPEG/%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8_%EB%8F%84%EC%8B%9D%ED%99%94.jpg?type=w1200'
+        leftImage.style.visibility = 'visible'
+      })
+
+      const imageClear = function () {
+        leftImage.style.visibility = 'hidden'
+        rightImage.style.visibility = 'hidden'
       }
+    },
+    methods: {
+      changePage(index) {
+        this.$router.push(this.pages[index])
+      },
     }
   }
 </script>
 
 <style scoped>
-  .eval-container {
-    padding-top: 4rem;
-    height: 100vh;
-    width: 100vw;
-    background: linear-gradient(to bottom right, skyblue, white);
+.eval-container {
+  padding-top: 4rem;
+  height: 100vh;
+  width: 100vw;
+  background: linear-gradient(to bottom right, skyblue, white);
+}
 
-    /* background-image: '@/assets/main.png'; */
-  }
+.intro {
+  font-family: HSYuji-Regular;
+}
 
-  .intro {
-    font-family: HSYuji-Regular;
-  }
+.card {
+  width: 80vw;
+  height: 60vh;
+  background-color: rgb(255, 255, 255);
+  border: 1px solid white;
+  border-radius: 10px;
+  margin: 0 auto;
+  /* opacity: 0.6; */
+  padding: 50px;
+}
 
-  .card {
-    width: 80vw;
-    height: 60vh;
-    background-color: rgb(255, 255, 255);
-    border: 1px solid white;
-    border-radius: 10px;
-    margin: 0 auto;
-    /* opacity: 0.6; */
-    padding: 50px;
-  }
-  .menu-container {
-    height: 10vh;
-    margin-top: 3vh;
-  }
-  .menu {
-    width: 20vw;
-    height: 10vh;
-    border: 1px solid hotpink;
-    text-align: center;
-    line-height: 10vh;
-    /* margin-top: 2rem; */
-    object-fit: cover;
-    /* filter: grayscale(100%) contrast(120%); */
-    box-shadow: 10px 15px 25px 0 rgba(0, 0, 0, .2);
-    display: block;
-    transition: all .5s cubic-bezier(0.645, 0.045, 0.355, 1);
-    margin-top: -10px;
-    background-color: hotpink;
-    border-radius: 8px;
-    cursor: pointer;
-    font-family: HSYuji-Regular;;
-    /* margin-bottom: 20px; */
-  }
+.menu-container {
+  height: 10vh;
+  margin-top: 3vh;
+  font-size: 1.2rem;
+}
 
-  .menu:hover {
-    box-shadow: 1px 1px 10px 0 rgba(0, 0, 0, .1);
-    margin-top: 0;
-  }
+.menu {
+  width: 20vw;
+  height: 10vh;
+  border: 1px solid hotpink;
+  text-align: center;
+  line-height: 10vh;
+  /* margin-top: 2rem; */
+  object-fit: cover;
+  /* filter: grayscale(100%) contrast(120%); */
+  box-shadow: 10px 15px 25px 0 rgba(0, 0, 0, .2);
+  /* display: block; */
+  transition: all .5s cubic-bezier(0.645, 0.045, 0.355, 1);
+  margin-top: -10px;
+  background-color: hotpink;
+  border-radius: 8px;
+  cursor: pointer;
+  font-family: HSYuji-Regular;
+  ;
+  /* margin-bottom: 20px; */
+}
 
-  /* 이미지로 버튼 눌림 효과 css */
-  /* .photo {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: block;
-  }
-
-  .photo img {
-    width: 200px;
-    height: 280px;
-    object-fit: cover;
-    filter: grayscale(100%) contrast(120%);
-    box-shadow: 10px 15px 25px 0 rgba(0, 0, 0, .2);
-    display: block;
-    transition: all .5s cubic-bezier(0.645, 0.045, 0.355, 1);
-    margin-top: -10px;
-  }
-
-  .photo:hover img {
-    box-shadow: 1px 1px 10px 0 rgba(0, 0, 0, .1);
-  }
-
-  .photo .glow-wrap {
-    overflow: hidden;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    margin-top: -10px;
-  }
-
-  .photo .glow {
-    display: block;
-    position: absolute;
-    width: 40%;
-    height: 200%;
-    background: rgba(255, 255, 255, .2);
-    top: 0;
-    filter: blur(5px);
-    transform: rotate(45deg) translate(-450%, 0);
-    transition: all .5s cubic-bezier(0.645, 0.045, 0.355, 1);
-  }
-
-  .photo:hover .glow {
-    transform: rotate(45deg) translate(450%, 0);
-    transition: all 1s cubic-bezier(0.645, 0.045, 0.355, 1);
-  }
-
-  .photo:hover img,
-  .photo:hover .glow-wrap {
-    margin-top: 0;
-  } */
+.menu:hover {
+  box-shadow: 1px 1px 10px 0 rgba(0, 0, 0, .1);
+  margin-top: 0;
+}
 </style>
