d181fcbe30ea58ef635e03a8f04587d46576bdde
jongs3030 <jongs3030@naver.com>
2021-09-28 01:58:56 +0900

    Fix: 검색 상세 모달 애니메이션 구현
    Former-commit-id: 879b5342729856fc1ede2050d4391d0cd2b0fcbb

--- frontend/src/components/search/SearchDetail.vue
+++ frontend/src/components/search/SearchDetail.vue
@@ -24,31 +24,32 @@
       <!-- 플립북 -->
       <section>
         <v-card>
-          <div id="flipbook1">
+          <div id="flipbook1" @click="goFlip" :style="flipCSS">
             <div style="">
               <v-img 
                 :src="searchDetailImageURL" 
                 alt="image" 
-                style="width: 50%; height: 50%; margin-left: 25%; margin-top: 10%"
+                style="width: 50%; height: 50%; margin-left: 25%; margin-top: 10%; border-radius: 20px"
               >
               </v-img>
-              <h1 style="margin-top: 10%">상세보기 -> </h1>
+              <h1 style="margin-top: 10%; font-family: 'Cafe24Ssurround'; color: #FBACCC;">상세보기 -> </h1>
             </div>
           </div>
-          <div id="flipbook2">
-            
+
+          <!-- 코디 정보 -->
+          <div id="flipbook2" @click="goFlip" :style="flipCSS2">
             <h1>옷 정보</h1>
             <h1>스타일 : 아방가르드</h1>
             <br>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta consequatur inventore quasi consequuntur in ipsam cum, non quam optio libero iure vel, dolore totam amet vero incidunt iusto suscipit voluptatibus.</p>
-            
           </div>
-          <v-card-actions>
+
+          <v-card-actions style="height: 5vh">
             <v-spacer></v-spacer>
             <v-btn
               color="secondary"
               text
-              @click="showStyleInfo({showSearchDetail})"
+              @click="showStyleInfo({showSearchDetail}); variableShowInitialize();"
             >
               <h1>X</h1>
             </v-btn>
@@ -106,6 +107,7 @@ export default {
   data () {
     return {
       dialog: false,
+      show: false,
       pages: ['@/assets/1.jpg',
               '@/assets/2.jpg',
               '@/assets/3.jpg',
@@ -116,12 +118,34 @@ export default {
     ...mapState([
       'showSearchDetail',
       'searchDetailImageURL'
-    ])
+    ]),
+    flipCSS () {
+      if (this.show) {
+        return {
+          transform: 'rotateY(-180deg) translateX(-17.5vw)',
+          background: '#DDDDDD',
+        }
+      }
+    },
+    flipCSS2 () {
+      if (this.show) {
+        return {
+          transform: 'translateX(17.5vw)',
+          transition: '1.5s'
+        }
+      }
+    }
   },
   methods: {
     ...mapActions([
       'showStyleInfo'
-    ])
+    ]),
+    goFlip () {
+      this.show = !this.show
+    },
+    variableShowInitialize () {
+      this.show = false
+    }
   }
 }
 </script>
@@ -132,36 +156,36 @@ section{
   position: relative;
   width: 100vw;
   height: 80vh;
-  perspective: 300vh;
+  perspective: 800vh;
 }
 #flipbook1 {
   margin:5% auto;
   position:absolute;
-  left: 50%;
+  left: 25%;
   width: 33vw; height: 70vh;
   text-align: center;
   box-shadow: 5px 5px 15px #333;
   background: #FFFFFF;
   transform-origin: left center;
-  transition: 2s;
+  transition: 1.5s;
   z-index:5;
 }
 
 #flipbook2{
   margin:5% auto;
   position:absolute;
-  left: 50%;
+  left: 25%;
   width: 33vw; height: 70vh;
   text-align: center;
   box-shadow: 5px 5px 15px #333;
   background: #cba;
   transform-origin: left center;
-  transition: 2s;   
+  transition: 1.5s;   
   z-index: 4;
 }
 #flipbook1:hover{
-  transform: rotateY(-180deg);
-  background: #DDDDDD;
+  transform: rotateY(-20deg);
+  background: #F1F1F1;
   
 }
 
