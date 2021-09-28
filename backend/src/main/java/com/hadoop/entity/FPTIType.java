fba384a41aa7374de97ecf6c059434974e57e69f
김준환 <vkffkehlss@gmail.com>
2021-09-28 14:57:22 +0900

    feature: 패피티아이 결과 조회 api 추가
    Former-commit-id: 8f6f0a210ca345c44bfb4e4415082be8dac157ef

---
+++ backend/src/main/java/com/hadoop/entity/FPTIType.java
@@ -0,0 +1,12 @@
+package com.hadoop.entity;
+
+import lombok.Setter;
+
+@Setter
+public class FPTIType {
+    private Integer typeNo;
+    private String fptiName;
+    private String desc;
+    private String iconUrl;
+    private String bgImgUrl;
+}
---
+++ backend/src/main/java/com/hadoop/service/FPTIServiceImpl.java
@@ -0,0 +1,21 @@
+package com.hadoop.service;
+
+import com.hadoop.entity.FPTIType;
+import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+@AllArgsConstructor
+public class FPTIServiceImpl implements FPTIService{
+
+    private final MongoTemplate mongoTemplate;
+
+    @Override
+    public FPTIType getResult(int typeNo) {
+        return mongoTemplate.findOne(
+                Query.query(Criteria.where("typeNo").is(typeNo)),
+                FPTIType.class
+        );
+    }
+}
