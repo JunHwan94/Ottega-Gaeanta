29337f78adb0dd7f4730feeb0c253eb437f0a044
정범진 <last9500@naver.com>
2021-09-28 15:24:46 +0900

    feat : 검색 상세 페이지의 유사 스타일 종류와 사진 URL 가져오기 기능 추가
    Former-commit-id: 757846f209e0cf726b7ef4a7eadd822effcb9715

---
+++ backend/src/main/java/com/hadoop/response/OtherStyleRes.java
@@ -0,0 +1,14 @@
+package com.hadoop.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OtherStyleRes {
+
+    String category;
+    String url;
+
+}
