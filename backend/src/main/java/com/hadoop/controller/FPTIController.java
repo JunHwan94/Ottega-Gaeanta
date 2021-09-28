cc444cf75c8e55ac8571ed165bd563b4b6f23b76
김준환 <vkffkehlss@gmail.com>
2021-09-28 15:59:34 +0900

    refactor : FPTIController swagger 설명 추가
    Former-commit-id: b424d1ecc803dddd12924f6f7640a0e238f915ac

--- backend/src/main/java/com/hadoop/controller/FPTIController.java
+++ backend/src/main/java/com/hadoop/controller/FPTIController.java
@@ -4,6 +4,8 @@ import com.hadoop.entity.FPTIType;
 import com.hadoop.response.BaseResponseBody;
 import com.hadoop.response.FPTIRes;
 import com.hadoop.service.FPTIService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +22,8 @@ public class FPTIController {
     private final FPTIService fptiService;
 
     @GetMapping("/result")
-    public ResponseEntity<? extends BaseResponseBody> getResult(int typeNo){
+    @ApiOperation(value = "패피티아이 유형 정보", notes = "패피티아이 유형 조회")
+    public ResponseEntity<? extends BaseResponseBody> getResult(@ApiParam(value = "패피티아이 유형 번호", required = true) int typeNo){
         FPTIType type = fptiService.getResult(typeNo);
         log.debug(type.toString());
         return ResponseEntity.status(200).body(FPTIRes.of(200, "성공", type));
