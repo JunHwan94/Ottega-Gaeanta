5ccdbd2ad35214793c523c583d1a7f19b2e6157d
김백준 <henry9489@gmail.com>
2021-10-01 13:47:08 +0900

    Fix : Fix Entity FPTItype
    Former-commit-id: f32c5b4eec3d2d39a8a091a635eccd7ff3055687

--- backend/src/main/java/com/hadoop/entity/FPTIType.java
+++ backend/src/main/java/com/hadoop/entity/FPTIType.java
@@ -3,12 +3,17 @@ package com.hadoop.entity;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document(collection = "fpti_types")
 public class FPTIType {
     private Integer typeNo;
     private String fptiName;
+    private String title;
     private String desc;
+    private String comment;
     private String iconUrl;
-    private String bgImgUrl;
+    private List<String> bgImgUrl;
+    private List<String> hashtag;
 }
