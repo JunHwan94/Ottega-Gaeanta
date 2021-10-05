df010484bb504caec71919633634cac08baad65e
김준환 <vkffkehlss@gmail.com>
2021-10-05 14:25:57 +0900

    refactor : BaseResponseBody 생성자 수정, 패키지 구조 수정
    Former-commit-id: 952fdda240b3c8a01299ded0c36017613ed5c6da

---
+++ backend/src/main/java/com/hadoop/api/eval/model/db/entity/Rank.java
@@ -0,0 +1,11 @@
+package com.hadoop.api.eval.model.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Rank {
+    String key;
+    String value;
+}
--- backend/src/main/java/com/hadoop/repository/EvalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hadoop.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvalRepository {
-}
